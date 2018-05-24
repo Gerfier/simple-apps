4336487ca5e2917df20c246a316b5b5f6472757a
Gerfier <gerrardf7@gmail.com>
2018-05-24 11:12:29 -0500

    remove note

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Some simple JavaScript applications
 
 You can run it using this link: https://cdn.rawgit.com/Gerfier/simple-apps/5fc1c514/index.html
 
-To fix:
--StickyNotes remove option
+To do:
+-Add apps imgs to cards
+
+
--- apps/stickyNotes/stickyNotes.js
+++ apps/stickyNotes/stickyNotes.js
@@ -1,26 +1,19 @@
-/*This file was created to test the posted jobs page
-Ensuring that a new job is added with the card layout and that even adding multiple
-cards still work with the grid properly and is responsive */
-
-//This function can be used or if you prefer your own with vanilla javascript syntax
 
 $('#newStickyNote-btn').click(function () {
 
     var noteTitle = $("#note-title").val();
     var noteCont = $("#note-content").val();
 
-    //Clear out text after posting job
+    //Clear out text after adding note
     $("#note-title").val("");
     $("#note-content").val("");
 
-    //append new job as first child
+    //append new note as first child
     $('#stickyNotes-row').prepend('<div id=sticky-note class="col-lg-3 col-md-6 col-sm-12">'
         + '<div class="card" mx-auto>'
         + '<div class="card-body">'
         + '<h4 class="card-title">' + noteTitle + '</h4>'
         + '<p class="card-text" style="padding-top: 3rem;">' + noteCont + '</p>'
-        //email of course will be modified later to be only visible when employer hires a student 
-        //otherwise nobody can see it
         + '</div>'
         + '<div class= "note-options" >'
         + '<span data-toggle="tooltip" data-placement="bottom" title="Delete Note" id="deleteNote-btn">'
@@ -30,15 +23,14 @@ $('#newStickyNote-btn').click(function () {
         + '</div>'
         + '</div>');
 
-    //hide modal once user clicks button to post job
+    //hide modal once user clicks button to add note
     $('#stickyNoteModal').modal('hide');
 
-    $('#deleteNote-btn').click(function () {
-        $('#sticky-note').remove();
-    });
+
 });
 
+//Remove note when user clicks trash icon
+    $(document).on("click", '#deleteNote-btn', function () {        
+            $(this).closest("#sticky-note").remove();
+    });
 
-$('#deleteNote-btn').click(function () {
-    $('#sticky-note').remove();
-})
